8fb99a0f4397968d4375de238be0ce49cf06c029
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-01 13:16:47 +0600

    added to new projects

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div>
-            <h2>this is login</h2>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="">
+                    <div className="text-center ">
+                        <h1 className="text-5xl font-bold mb-8">Register now!</h1>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                                <p className='mt-4'>Creat New acount ? Please <Link to='/register' className="btn btn-link">Register</Link></p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
